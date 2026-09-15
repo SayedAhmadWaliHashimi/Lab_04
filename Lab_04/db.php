ce3048ef7d843e914e150576bc9b0fe272176c5c
SayedAhmadWali <hashimiahmadwali95@gmail.com>
2026-09-14 21:59:58 -0700

    Update db.php

--- Lab_04/db.php
+++ Lab_04/db.php
@@ -1,1 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "admission_db";
+$port = 3307;
 
+// Create connection using OOP MySQLi
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Set charset to utf8mb4
+$conn->set_charset("utf8mb4");
+?>
